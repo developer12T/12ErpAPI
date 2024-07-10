7ff0e65fa6e1af0dac79216a656bb18d5f3df930
Nopparat12T <nopparat.mun@onetwotrading.co.th>
2024-07-10 09:30:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// Routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const customersRouter = require('./routes/customer');
+
+//import middleware
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//  Set Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/customer', customersRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
