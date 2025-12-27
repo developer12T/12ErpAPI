66ed018a6c36141f6e715e5a38b319f0d220f078
Nopparat Munsuwan <nopparat.mun@onetwotrading.co.th>
2025-12-27 16:44:06 +0700

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,30 +5,30 @@ const { JWT_SECRET } = require('../config/index')
 // ✅ ใส่ path แบบเต็ม หรือแค่ prefix ก็ได้
 const openPaths = ['/login', '/erp/users/login']
 
-module.exports = function (req, res, next) {
-  // ✅ ถ้า path เริ่มด้วย path ที่ยกเว้น → ข้ามได้
-  if (openPaths.some(p => req.path.startsWith(p))) {
-    return next()
-  }
+// module.exports = function (req, res, next) {
+//   // ✅ ถ้า path เริ่มด้วย path ที่ยกเว้น → ข้ามได้
+//   if (openPaths.some(p => req.path.startsWith(p))) {
+//     return next()
+//   }
 
-  const authHeader = req.headers['authorization']
-  if (!authHeader) {
-  return res.status(401).json({ 
-    message: 'Access denied. Missing authentication token.' 
-  })
-  }
+//   const authHeader = req.headers['authorization']
+//   if (!authHeader) {
+//     return res.status(401).json({
+//       message: 'Access denied. Missing authentication token.'
+//     })
+//   }
 
-  const token = authHeader.split(' ')[1]
-  if (!token) {
-    return res.status(401).json({ message: 'Invalid token format' })
-  }
+//   const token = authHeader.split(' ')[1]
+//   if (!token) {
+//     return res.status(401).json({ message: 'Invalid token format' })
+//   }
 
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET)
-    req.user = decoded
-    next()
-  } catch (err) {
-    console.error('❌ Invalid token:', err.message)
-    res.status(403).json({ message: 'Invalid or expired token' })
-  }
-}
+//   try {
+//     const decoded = jwt.verify(token, JWT_SECRET)
+//     req.user = decoded
+//     next()
+//   } catch (err) {
+//     console.error('❌ Invalid token:', err.message)
+//     res.status(403).json({ message: 'Invalid or expired token' })
+//   }
+// }
